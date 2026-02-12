f40c4f7f4de91b2c938df472d2caf583a277d0bb
repinek <137826826+repinek@users.noreply.github.com>
2026-02-12 16:05:48 +0500

    fix(constants): fix keys to snake_case

--- LegacyGram/data/constants.py
+++ LegacyGram/data/constants.py
@@ -5,21 +5,22 @@ GITHUB_URL = "https://github.com/repinek/LegacyGram"
 # But it's looks overall better and etg uses the same format (consistency)
 class Keys:
     class General:
-        toggleSwitchAll = "toggleSwitchAll"
-        hidePremiumRow = "hidePremiumRow"
-        hideStarsRow = "hideStarsRow"
-        hideTonRow = "hideTonRow"
-        hideBusinessRow = "hideBusinessRow"
-        hideSendAGiftRow = "hideSendAGiftRow"
+        switch_all = "switchAll"
+        hide_premium_row = "hidePremiumRow"
+        hide_stars_row = "hideStarsRow"
+        hide_ton_row = "hideTonRow"
+        hide_business_row = "hideBusinessRow"
+        hide_send_a_gift_row = "hideSendAGiftRow"
+        drawer_options = "drawerSettings"  # ETG
 
     class Premium:
-        replyElements = "replyElements"
-        hideStories = "hideStories"
-        hideActionBarStatus = "hideActionBarStatus"
-        hideStoriesTab = "hideStoriesTab"
+        reply_elements = "replyElements"  # ETG
+        hide_stories = "hideStories"  # ETG
+        hide_action_bar_status = "hideActionBarStatus"  # ETG
+        hide_stories_tab = "hideStoriesTab"
 
     class Gifts:
-        hideBottomGiftButton = "hideBottomGiftButton"
-        hideGiftsTab = "hideGiftsTab"
-        hideStarsRating = "hideStarsRating"
-        hideActionBarSendGift = "hideActionBarSendGift"
+        hide_bottom_gift_button = "hideBottomGiftButton"
+        hide_gifts_tab = "hideGiftsTab"
+        hide_stars_rating = "hideStarsRating"
+        hide_action_bar_send_gift = "hideActionBarSendGift"
