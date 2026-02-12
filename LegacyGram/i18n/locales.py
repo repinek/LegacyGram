d1dda52bdbda86d52179606689d385d0c25c3daf
repinek <137826826+repinek@users.noreply.github.com>
2026-02-12 16:07:03 +0500

    feat(i18n): add en strings

--- LegacyGram/i18n/locales.py
+++ LegacyGram/i18n/locales.py
@@ -1,1 +1,53 @@
-STRINGS = {"ru": {}, "en": {}}
+# I wanted to do it's camelCase and using Keys from constants but idk...
+# fmt: off
+STRINGS = {
+    "ru": {
+        # --- General Sub-Page ---
+        # --- Premium Sub-Page ---
+        # --- Gifts Sub-Page ---
+        # --- About Sub-Page ---
+        "github_repository": "ахаха",
+        # --- Main Settings List ---
+    },
+    "en": {
+        # --- General Sub-Page ---
+        "settings_options": "Settings Options",
+        "switch_all": "Switch All",
+        "hide_premium_row": "Hide Telegram Premium",
+        "hide_stars_row": "Hide My Stars",
+        "hide_ton_row": "Hide My TON",
+        "hide_business_row": "Hide Telegram Business",
+        "hide_send_a_gift_row": "Hide Send a Gift",
+        "drawer_options": "Drawer Options",
+        "manage_drawer_options": "Manage Drawer Options",
+
+        # --- Premium Sub-Page ---
+        "profile_color": "Profile Color",
+        "manage_reply_elements": "Manage Reply Elements",
+        "chat_list": "Chat List",
+        "hide_stories": "Hide Stories",
+        "hide_action_bar_status": "Hide Status",
+        "profile_tabs": "Profile Tabs",
+        "hide_stories_tab": "Hide Stories Tab in Profile",
+
+        # --- Gifts Sub-Page ---
+        "gifts_header": "Gifts Tabs, Buttons and etc...",
+        "hide_bottom_gift_button": "Hide Gift Button",
+        "hide_gifts_tab": "Hide Gifts Tab",
+        "hide_stars_rating": "Hide Stars Rating",
+        "hide_action_bar_send_gift": "Hide Send Gift button from Action Bar",
+
+        # --- About Sub-Page ---
+        "client_version": "Client version: {0}",
+        "github_repository": "Github Repository",
+        "github_info": "Modular architecture with custom build script, Ruff linting, and typing stubs for java and android modules.",
+
+        # --- Main Settings List ---
+        "settings_related_to": "Setting related to...",
+        "general": "General",
+        "premium": "Premium",
+        "gifts": "Gifts, Stars and TON",
+        "about": "About Plugin",
+    }
+}
+# fmt: on
