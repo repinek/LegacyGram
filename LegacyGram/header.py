d48cf84dcb73263c7406f9353a2f6e14b6d58f29
repinek <137826826+repinek@users.noreply.github.com>
2026-04-25 20:35:43 +0500

    chore: version bump to 1.2.0, add todo's

--- LegacyGram/header.py
+++ LegacyGram/header.py
@@ -1,6 +1,6 @@
 __name__ = "LegacyGram"
 __description__ = "A highly customizable plugin to **remove paid and bloated features** from Telegram.\n**Recommended version:** 12.1.1"
-__version__ = "1.1.7"
+__version__ = "1.2.0"
 __id__ = "legacygram"
 __author__ = "@wepinek"
 __icon__ = "LegacyGram/0"
@@ -11,4 +11,6 @@ TODO:
 Remove posts and archived posts from My profile tab
 Remove tags from Saved Messages
 suggestion: remove premium Emojis from selection
+Update checking
+Settings menu on newer versions (with new "liquid glass" design)
 """
